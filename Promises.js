91645bcfb17561f3b1a082e8c0b86a18ebd64433
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2023-12-01 18:45:30 +0530

    Learn Promises

---
+++ Promises.js
@@ -0,0 +1,15 @@
+let p = new Promise((resolve, reject) => {
+  let a = 1 + 1;
+
+  if (a == 2) {
+    resolve("Success");
+  } else {
+    reject("Failed");
+  }
+});
+
+p.then((message) => {
+  console.log("This is in the then " + message);
+}).catch((message) => {
+  console.log("This is in the catch " + message);
+});
