ffca322363267e36bd656938ab03826905423402
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-03-31 00:03:52 +0530

    Exam Questions

---
+++ Application Frameworks/Q1.js
@@ -0,0 +1,24 @@
+// Assume Vehicle class has already been implemented
+function Vehicle() {
+  // Constructor logic for Vehicle
+}
+
+// Car class extending Vehicle
+function Car() {
+  // Constructor logic for Car
+}
+
+// Set Car prototype to be the same as Vehicle prototype
+Car.prototype = Object.create(Vehicle.prototype);
+Car.prototype.constructor = Car;
+
+// Define a method getCar for Car prototype
+Car.prototype.getCar = function () {
+  console.log("Hello");
+};
+
+// Create a new instance of Car
+const car = new Car();
+
+// Call the getCar method on the car object
+car.getCar();
---
+++ Application Frameworks/Q2.js
@@ -0,0 +1,8 @@
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add(3, 4, 5));
+
+// What will the code output?
+// Answer: 7
