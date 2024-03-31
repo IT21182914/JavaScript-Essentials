30d8790cb14190e9d4aaf607fe3d0ee9be004dac
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-03-31 15:48:59 +0530

    Questions

--- Application Frameworks/Q3.js
+++ Application Frameworks/Q3.js
@@ -2,10 +2,10 @@ function Vehicle(model) {
   this.model = model; // Corrected assignment
 }
 
-function Car(model) {}
+function Car(model) {} //since this constructor is empty, it is not necessary to define it.so undefined
 
 Car.prototype = Object.create(Vehicle.prototype);
 Car.prototype.constructor = Car;
 
 var car = new Car("car");
-console.log(car.model); // Output: car
+console.log(car.model); // Output: undefined
---
+++ Application Frameworks/Q5.js
@@ -0,0 +1,11 @@
+let a = "";
+
+if (a) {
+} else {
+  console.log("a is false");
+}
+
+// var a = 1
+// var a = 'false'
+// var a = ''    ---------> Answer is:  a is false
+// none of the above
