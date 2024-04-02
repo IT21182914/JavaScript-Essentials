1406fe892b7c44301c1738aa7e4b08ac172804b8
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-04-02 11:37:38 +0530

    ClosureExample 2

--- ClosureExample.js
+++ ClosureExample.js
@@ -18,4 +18,4 @@ counter.increment(); // Output: Count: 1
 counter.increment(); // Output: Count: 2
 counter.decrement(); // Output: Count: 1
 
-// In the above example, the createCounter function returns an object with two methods increment and decrement.
+
